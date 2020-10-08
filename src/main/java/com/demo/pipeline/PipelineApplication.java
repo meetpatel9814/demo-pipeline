2c984ad495d495ca6905cb0408d36d8360cda989
Meet Patel <Meet.Patel@dpworld.com>
2020-10-08 19:17:25 +0530

    intial

---
+++ src/main/java/com/demo/pipeline/PipelineApplication.java
@@ -0,0 +1,25 @@
+package com.demo.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@ComponentScan(value = "com.demo.pipeline")
+@RestController
+public class PipelineApplication {
+
+
+	@GetMapping("/")
+	public String defaultMethod() {
+		return "HOME PAGE";
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PipelineApplication.class, args);
+	}
+
+}
